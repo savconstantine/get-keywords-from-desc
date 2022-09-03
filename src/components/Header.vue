8570f25ee02bfdf87317a3450109bfe746d237fb
Konstantin Savusia <savconstantine@gmail.com>
2022-09-04 02:38:29 +0600

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <header>
+    <div class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 py-8 lg:py-16">
+      <h1 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Get Keywords
+        From Description</h1>
+    </div>
+  </header>
+</template>
