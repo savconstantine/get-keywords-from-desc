c2933caed53a852c4e5aae1d8aa9214087da7714
Konstantin Savusia <savconstantine@gmail.com>
2022-09-04 02:42:43 +0600

    config deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  base: process.env.NODE_ENV === 'production'
+    ? '/get-keywords-from-desc/' // prod
+    : '/', // dev
+  plugins: process.env.NODE_ENV === 'production' ? [vue({ reactivityTransform: true }), svgLoader()] : [vue(), svgLoader()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
