645a73e6208822cbb0df2f94beb466223956d86f
Konstantin Savusia <savconstantine@gmail.com>
2022-09-27 22:14:50 +0600

    add dark-white mode

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <script setup>
+import { ref } from "vue";
 import { RouterLink, RouterView } from "vue-router";
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
+
+const changeDarkLightMode = () => {
+  isDark.value = !isDark.value;
+  localStorage.setItem("darkMode", isDark.value);
+};
+
+const hasDarkModeSetted = () => {
+  if (localStorage.getItem("darkMode")) {
+    return localStorage.getItem("darkMode") == "true";
+  }
+
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: light)").matches
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+let isDark = ref(hasDarkModeSetted());
 </script>
 
 <template>
-  <Header />
-  <RouterView />
-  <Footer />
+  <div :class="isDark ? 'dark' : ''" class="antialiased">
+    <div
+      class="bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100 min-h-screen flex flex-col justify-between"
+    >
+      <Header @toggleDarkMode="changeDarkLightMode" :isDark="isDark" />
+      <RouterView />
+      <Footer />
+    </div>
+  </div>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,38 @@
+<script setup>
+  import MoonSvg from "@/assets/dark-light-mode/moon.svg?component";
+  import SunSvg from "@/assets/dark-light-mode/sun.svg?component";
+
+
+  const props = defineProps({
+    isDark: {
+      type: Boolean,
+      required: true
+    },
+  });
+
+  const emits = defineEmits(["toggleDarkMode"]);
+
+</script>
+
+
 <template>
   <header>
-    <div class="bg-white border-gray-200 px-4 lg:px-6 pt-2.5 dark:bg-gray-800 lg:pt-14">
+    <div class="bg-white border-gray-200 px-4 lg:px-6 pt-2.5 dark:bg-gray-800 lg:pt-14 flex flex-col">
       <h1 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Get Keywords
         From Description</h1>
+        <button
+            @click="emits('toggleDarkMode')"
+            class="flex items-center justify-center w-8 h-8 focus:outline-none shadow-xl shadow-gray-400 rounded-full uppercase bg-gradient-to-r from-teal-400 to-teal-800 text-white self-center"
+          >
+          <transition name="fade" mode="out-in">
+            <template v-if="props.isDark">
+              <SunSvg class="w-4" />
+            </template>
+            <template v-else>
+              <MoonSvg class="w-4" />
+            </template>
+          </transition>
+          </button>
     </div>
   </header>
 </template>
