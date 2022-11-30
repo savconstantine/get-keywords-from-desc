e047d5d1eea69d2a3f320389526b576f40f26904
Konstantin Savusia <savconstantine@gmail.com>
2022-11-30 21:16:30 +0600

    show error depends of error state in the store

--- src/components/CopyKeywordsBtn.vue
+++ src/components/CopyKeywordsBtn.vue
@@ -2,7 +2,11 @@
   <div class="w-full">
     <button @click.stop.prevent="copyKeywords" type="button"
       class="w-full py-3 px-5 text-sm font-medium text-center text-white rounded-lg  focus:ring-4 focus:outline-none transition-all ease-in duration-300"
-      :class="{ 'bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800': copyButtonTextState === 'default', 'bg-green-500 hover:bg-green-600 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800': copyButtonTextState === 'copied', 'bg-red-500 hover:bg-red-600 focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-700': copyButtonTextState === 'error' || copyButtonTextState === 'noKeywords' }">
+      :class="{
+        'bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800': copyButtonTextState === ERROR_STATE_DEFAULT,
+        'bg-green-500 hover:bg-green-600 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800': copyButtonTextState === ERROR_STATE_COPIED,
+        'bg-red-500 hover:bg-red-600 focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-700': copyButtonTextState === ERROR_STATE_ERROR || copyButtonTextState === ERROR_STATE_NOKEYWORDS
+      }">
       {{ copyButtonText }}
     </button>
     <input id="block-to-copy" type="hidden" :value="keywords" />
@@ -11,7 +15,7 @@
 
 <script>
 import { storeToRefs, mapActions } from "pinia";
-import { useErrorsStore } from "../stores/errors";
+import { useErrorsStore, ERROR_STATE_DEFAULT, ERROR_STATE_COPIED, ERROR_STATE_NOKEYWORDS, ERROR_STATE_ERROR } from "../stores/errors";
 
 export default {
   props: {
@@ -27,6 +31,10 @@ export default {
     return {
       copyButtonTextState: getCopyButtonTextState,
       copyButtonText: getCopyButtonText,
+      ERROR_STATE_DEFAULT,
+      ERROR_STATE_COPIED,
+      ERROR_STATE_NOKEYWORDS,
+      ERROR_STATE_ERROR
     };
   },
   methods: {
@@ -38,24 +46,24 @@ export default {
         blockToCopy.select();
         try {
           document.execCommand("copy");
-          this.setCopyButtonTextState('copied');
+          this.setCopyButtonTextState(ERROR_STATE_COPIED);
           setTimeout(() => {
-            this.setCopyButtonTextState('default');
+            this.setCopyButtonTextState(ERROR_STATE_DEFAULT);
           }, 5000);
         } catch (err) {
           alert("Oops, unable to copy");
-          this.setCopyButtonTextState('error');
+          this.setCopyButtonTextState(ERROR_STATE_ERROR);
           setTimeout(() => {
-            this.setCopyButtonTextState('default');
+            this.setCopyButtonTextState(ERROR_STATE_DEFAULT);
           }, 10000);
         }
         /* unselect the range */
         blockToCopy.setAttribute("type", "hidden");
         window.getSelection().removeAllRanges();
       } else {
-        this.setCopyButtonTextState('noKeywords');
+        this.setCopyButtonTextState(ERROR_STATE_NOKEYWORDS);
         setTimeout(() => {
-          this.setCopyButtonTextState('default');
+          this.setCopyButtonTextState(ERROR_STATE_DEFAULT);
         }, 5000);
       }
     },
